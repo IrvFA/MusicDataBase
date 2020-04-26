15c4c5d1dec2160ed22555657c441c9c42d95acd
jaimi <a01374696@itesm.mx>
2020-04-25 19:37:42 -0500

    Aprendiendo a usar Object Stores

---
+++ musicDB.js
@@ -0,0 +1,21 @@
+import Dexie from 'dexie';
+
+/*
+Team Members:
+Irving Fuentes Aguilera 
+Jaime Orlando López Ramos
+Joaquín Ríos Corvera
+Objective: Apply our DB knowledge to implement an indexed Database
+*/
+
+const musicDB = new Dexie("Irving's Music Palace Condesa"); // Declaration of our DB var
+
+// Definition of tables with their respective
+musicDB.version(1).stores({
+    Client: '++id, name, phone_no, email',
+    Label: '++id, name, country',
+    
+
+
+});
+
