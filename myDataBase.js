b3b8e36ca6b804ac958421144b397b00420cb2fc
Irving <irvingfa72@gmail.com>
2020-04-25 20:40:14 -0500

    Second Test

--- myDataBase.js
+++ myDataBase.js
@@ -5,5 +5,23 @@ var db = new Dexie('MusicStoreDB');
 
 
 db.version(1).stores({
-    
+    Album: "++albumID, title, labelID, genre, releaseYear, price, stock",
+    Label: "++labelID, name, country",
+    Artists: "++artistID, [firstName +lastName], nationality, nationality, birthDate",
+    Client: "++clientID, [firstName + lastName], telephone, email",
+    Producer: "++producerID; [firstName + lastName], nationality"
+});
+
+var Album = db.Album.defineClass ({
+    albumID: Number,
+    title: String,
+    label: Label,
+    genre: String,
+    releaseYear: Date,
+    price: Number,
+    stock: Number,
+})
+
+var Label = db.Label.defineClass ({
+    labelID: 
 })
