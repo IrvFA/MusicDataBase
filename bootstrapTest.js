59c4f5fb44b5ca7a6241ddfe14387692ac58ce29
Jose Antonio <A01746585@itesm.mx>
2020-05-08 21:56:31 -0500

    Intento de delete

--- bootstrapTest.js
+++ bootstrapTest.js
@@ -44,6 +44,7 @@ function addData(db) {
         releaseYear: $("#releaseYear").val(),
         price: $("#price").val(),
         stock: $("#stock").val()
+      
     })
         .then(function (docRef) {
             console.log("Document written with ID: ", docRef.id);
@@ -55,3 +56,43 @@ function addData(db) {
         });
     return this.some_flag_variable;
   }
+
+  function removeData(db) {
+    $("#demo").html("Welcome");
+    db.collection("Albums").remove({
+        title: $("#albumName").val(),
+        artist: $("#artistName").val(),
+        genre: $("#genre").val(),
+        releaseYear: $("#releaseYear").val(),
+        price: $("#price").val(),
+        stock: $("#stock").val()
+        $(".deleteBtn").bind("click", Delete)
+    })
+        .then(function (docRef) {
+            console.log("Document written with ID: ", docRef.id);
+            $("#alert").empty();
+            $('.parent').append("<div class='alert alert-success alert-dismissable'><button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;</button>Success! message sent successfully.</div>")
+        })
+        .catch(function (error) {
+            console.error("Error deleting document: ", error);
+        });
+    return this.some_flag_variable;
+  }
+  var adaRef = firebase.database(db).ref(docRef.id);
+adaRef.remove()
+  .then(function() {
+    console.log("Remove succeeded.")
+  })
+  .catch(function(error) {
+    console.log("Remove failed: " + error.message)
+  });
+  
+  $("body").on("click", ".btn-delete", function(){
+    $("demo").parents("tr").remove();
+    
+function Delete(){
+    var par = $(this).parent("#demo").parent()
+    par.remove()
+}
+}
+}
