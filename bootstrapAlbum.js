9d028da123062bf1305acacdb621d98b28363426
joaquinrios <30807767+joaquinrios@users.noreply.github.com>
2020-05-10 01:26:43 -0500

    Button implementations
    Modify button on Albums.
    Switch page button on Artists.
    
    Modify button now updates text fields.

--- bootstrapAlbum.js
+++ bootstrapAlbum.js
@@ -56,19 +56,67 @@ function updateTable() {
                 <td>${doc.data().releaseYear}</td>
                 <td>${doc.data().price}</td>
                 <td>${doc.data().stock}</td>
-                <td><button class = "btn btn-danger" onclick = "deleteArtist('${doc.id}')">Delete</button></td>
-                <td><button class = "btn btn-warning">Modify</button></td>
+                <td><button class = "btn btn-danger" onclick = "deleteAlbum('${doc.id}')">Delete</button></td>
+                <td><button class = "btn btn-warning" onclick = "updateAlbum('${doc.id}','${doc.data().title}', '${doc.data().artist}', '${doc.data().genre}', '${doc.data().releaseYear}', '${doc.data().price}', '${doc.data().stock}')">Modify</button></td>
               </tr>
                 `
         });
     });
 }
 
-function deleteArtist(id) {
+function deleteAlbum(id) {
     db.collection("Albums").doc(id).delete().then(function () {
         console.log("Document successfully deleted!");
         updateTable();
     }).catch(function (error) {
         console.error("Error removing document: ", error);
     });
+}
+
+//Update firebase
+
+function updateAlbum(id, title, artist, genre, releaseYear, price, stock){
+
+    document.getElementById("albumName").value = title;
+    document.getElementById("artistName").value = artist;
+    document.getElementById("genre").value = genre;
+    document.getElementById("releaseYear").value = releaseYear;
+    document.getElementById("price").value = price;
+    document.getElementById("stock").value = stock;
+    var button = document.getElementById("addBtn1");
+    button.innerHTML = "Update";
+
+    button.onclick = function(){
+
+        var currentAlbum = db.collection("Albums").doc(id);
+
+
+        return currentAlbum.update({
+            title: $("#albumName").val(),
+            artist: $("#artistName").val(),
+            genre: $("#genre").val(),
+            releaseYear: $("#releaseYear").val(),
+            price: $("#price").val(),
+            stock: $("#stock").val()
+        })
+        .then(function() {
+            console.log("Document successfully updated!");
+            button.innerHTML = "Add";
+            document.getElementById("albumName").value = "";
+            document.getElementById("artistName").value = "";
+            document.getElementById("genre").value = "";
+            document.getElementById("releaseYear").value = "";
+            document.getElementById("price").value = "";
+            document.getElementById("stock").value = "";
+
+            updateTable();
+        })
+        .catch(function(error) {
+            // The document probably doesn't exist.
+            console.error("Error updating document: ", error);
+        });
+
+    }
+
+
 }
