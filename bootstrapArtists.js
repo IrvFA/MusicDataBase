5cca04efe555e0a02cc2c578069f92bd49bdb4b1
Joaquin Rios <30807767+joaquinrios@users.noreply.github.com>
2020-05-09 21:41:48 -0500

    Add new artists to firebase

---
+++ bootstrapArtists.js
@@ -0,0 +1,52 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyBwcS67sfF1ucVVXoJhADAu1FvU4A4vL-0",
+    authDomain: "musicstoredb.firebaseapp.com",
+    databaseURL: "https://musicstoredb.firebaseio.com",
+    projectId: "musicstoredb",
+    storageBucket: "musicstoredb.appspot.com",
+    messagingSenderId: "1062287219601",
+    appId: "1:1062287219601:web:a1f76b94b6685c360b5830",
+    measurementId: "G-05R68BMFPF"
+};
+
+
+$(document).ready(function () {
+    // all custom jQuery will go here
+    firebase.initializeApp(firebaseConfig);
+    var db = firebase.firestore();
+    $("#artistForm").submit(function() {
+        $("#demo").html("Welcome");
+        db.collection("Artists").add({
+            name: $("#artistName").val(),
+            birthday: $("#birthday").val().toString(),
+            nationality: $("#nationality").val()
+  
+        })
+            .then(function (docRef) {
+                console.log("Document written with ID: ", docRef.id);
+                
+            })
+            .catch(function (error) {
+                console.error("Error adding document: ", error);
+            });
+        return this.some_flag_variable;
+      });
+});
+
+function addData(db) {
+    $("#demo").html("Welcome");
+    db.collection("Artists").add({
+        name: $("#artistName").val(),
+        birthday: $("#birthday").val().toString(),
+        nationality: $("#nationality").val()
+    })
+        .then(function (docRef) {
+            console.log("Document written with ID: ", docRef.id);
+            $("#alert").empty();
+            $('.parent').append("<div class='alert alert-success alert-dismissable'><button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;</button>Success! message sent successfully.</div>")
+        })
+        .catch(function (error) {
+            console.error("Error adding document: ", error);
+        });
+    return this.some_flag_variable;
+  }
